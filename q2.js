7287ca98b0afd50ffdc67191de6eaf07d23189f0
Militus <imilitus@gmail.com>
2021-01-20 21:14:15 +0000

    q2 and q3 solution

---
+++ q2.js
@@ -0,0 +1,11 @@
+let oddID = []; 
+  let evenID = [];
+  for (let i = 1; i <= 100; i++) {
+    if (i % 2 === 0) {
+      evenID.push(i);
+      //console.log(evenID.length);      
+    }else {
+      oddID.push(i);
+    console.log(oddID.length);
+    }
+  }
